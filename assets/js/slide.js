c386d1db5c05d478f8634a9d7b5480187fd2fc0b
philiguchi19 <122083576+philiguchi19@users.noreply.github.com>
2023-01-30 20:23:40 +0300

    Update slide.js

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,10 +1,1 @@
-let leftArrow = document.querySelector(".fa-angle-left");
-let rightArrow = document.querySelector(".fa-angle-right");
-let cardColumns = document.querySelector(".featuredcard-columns");
 
-leftArrow.addEventListener("click", function(){
-    cardColumns.scrollLeft -= 50;
-});
-rightArrow.addEventListener("click", function(){
-    cardColumns.scrollLeft += 50;
-});
